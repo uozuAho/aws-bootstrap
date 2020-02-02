12da3388cb46aeb383bfc74e787508b4a7fda086
Warwick Stone <uozu.aho@gmail.com>
2020-02-02 12:09:30 +1100

    change dir in after_install, run as user

--- after_install.sh
+++ after_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Codedeploy doesn't preserve symlinks.
 # pm2 binary doesn't work unless it's a symlink to the node_modules/pm2 dir.
-# So, make pm2 binary a symlink:
-rm ./node_modules/.bin/pm2
-ln -s ./node_modules/pm2/bin/pm2 ./node_modules/.bin/pm2
+# So, here we make pm2 binary a symlink.
+
+source /home/ec2-user/.bash_profile
+cd /home/ec2-user/app/release
+rm node_modules/.bin/pm2
+ln -s node_modules/pm2/bin/pm2 node_modules/.bin/pm2
