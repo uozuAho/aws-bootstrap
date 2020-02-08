87d21dec2253fd27b7a497911015256d72fb1c6b
Warwick Stone <uozu.aho@gmail.com>
2020-02-08 20:17:51 +1100

    add vpc, multi az, load balancer

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -51,7 +51,7 @@ create_stack() {
     if [ $? -eq 0 ]; then
         aws cloudformation list-exports \
             --profile awsbootstrap \
-            --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
+            --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
     fi
 }
 
