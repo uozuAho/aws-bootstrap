533f16db7d275f09a1320257e075dac488109209
Warwick Stone <uozu.aho@gmail.com>
2020-02-13 21:38:10 +1100

    extract nested stack from main.yml
    - main is the top level
    - stage is an environment (will rename this)
    - deploy-infra.sh uploads stack to s3

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -13,6 +13,7 @@ STACK_NAME=awsbootstrap
 SETUP_STACK_NAME=$STACK_NAME-setup
 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
+CLOUDFORMATION_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 create_setup_stack() {
@@ -26,7 +27,28 @@ create_setup_stack() {
         --no-fail-on-empty-changeset \
         --capabilities CAPABILITY_NAMED_IAM \
         --parameter-overrides \
-        CodePipelineBucket=$CODEPIPELINE_BUCKET
+            CodePipelineBucket=$CODEPIPELINE_BUCKET
+            CloudFormationBucket=$CLOUDFORMATION_BUCKET
+}
+
+package_main() {
+    # put main template into the setup s3 bucket
+    echo -e "\n\n=========== Packaging main.yml ==========="
+    
+    mkdir -p ./cfn_output
+    
+    PACKAGE_ERR="$(aws cloudformation package \
+        --region $REGION \
+        --profile $CLI_PROFILE \
+        --template main.yml \
+        --s3-bucket $CFN_BUCKET \
+        --output-template-file ./cfn_output/main.yml 2>&1)"
+    
+    if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+        echo "ERROR while running 'aws cloudformation package' command:"
+        echo $PACKAGE_ERR
+        exit 1
+    fi
 }
 
 create_stack() {
@@ -36,7 +58,7 @@ create_stack() {
         --region $REGION \
         --profile $CLI_PROFILE \
         --stack-name $STACK_NAME \
-        --template-file main.yml \
+        --template-file ./cfn_output/main.yml \
         --no-fail-on-empty-changeset \
         --capabilities CAPABILITY_NAMED_IAM \
         --parameter-overrides \
@@ -56,25 +78,25 @@ create_stack() {
 }
 
 check_stacks() {
-    aws cloudformation describe-stacks --stack-name $SETUP_STACK_NAME
-    aws cloudformation describe-stacks --stack-name $STACK_NAME
+    aws cloudformation describe-stacks --profile $CLI_PROFILE --stack-name $SETUP_STACK_NAME
+    aws cloudformation describe-stacks --profile $CLI_PROFILE --stack-name $STACK_NAME
 }
 
 delete_stack() {
     echo "deleting stack '$STACK_NAME'"
-    aws cloudformation delete-stack --stack-name $STACK_NAME
+    aws cloudformation delete-stack --profile $CLI_PROFILE --stack-name $STACK_NAME
 }
 
 delete_setup_stack() {
     echo "deleting stack '$SETUP_STACK_NAME'"
-    aws cloudformation delete-stack --stack-name $SETUP_STACK_NAME
+    aws cloudformation delete-stack --profile $CLI_PROFILE --stack-name $SETUP_STACK_NAME
 }
 
 # ----------------------------------------------------------------------
 # run stuff!
 
-create_setup_stack
-create_stack
+# create_setup_stack
+# create_stack
 # check_stacks
-# delete_stack
+delete_stack
 # delete_setup_stack
